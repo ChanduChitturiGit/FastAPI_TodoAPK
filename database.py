a10c0a8e6b6e2add327593abc804ffc6804c58ce
Chandu Chitturi <103096269+ChanduChitturiGit@users.noreply.github.com>
2023-07-07 16:35:53 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:root@localhost/tests"
+SQLALCHEMY_DATABASE_URL = "postgresql://username:password@localhost/db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
